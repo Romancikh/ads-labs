394b3823aa73d70adb93654f7190ea9da4857a1c
Kutimskii Roman <kutimskiy.roma@gmail.com>
2023-09-18 11:16:12 +0300

    Add file opening

--- GroupManager/main.cpp
+++ GroupManager/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <windows.h>
+#include <fstream>
 
 int main(int argc, char *argv[]) {
     SetConsoleCP(1251);
@@ -9,5 +10,11 @@ int main(int argc, char *argv[]) {
     std::string inputPath = "input.txt";
     if (argc == 3 && strcmp(argv[1], "-f") == 0) {
         inputPath = argv[2];
+    }
+
+    std::ifstream inputFile(inputPath);
+    if (!inputFile.is_open()) {
+        std::cerr << "Не удалось открыть файл для чтения: " << inputPath << std::endl;
+        return 1;
     }
 }
